554aa251ffe6d21f7d25dede73d2b388c30ebc99
luizzzxt <luiznetotinho@gmail.com>
2026-09-15 10:38:04 -0300

    implementa verificacao de complexidade de senha

--- main.c
+++ main.c
@@ -25,5 +25,41 @@ void mascarar_dados(char dado[]) {
         dado[i] = '*';
     }
 }
+// 3. Validacao de complexidade de senha
+int validar_senha(char senha[]) {
+    int tamanho = strlen(senha);
+    if (tamanho < 8) return 0;
 
+    int tem_maiuscula = 0;
+    int tem_minuscula = 0;
+    int tem_digito = 0;
+
+    for (int i = 0; i < tamanho; i++) {
+        if (isupper(senha[i])) tem_maiuscula = 1;
+        else if (islower(senha[i])) tem_minuscula = 1;
+        else if (isdigit(senha[i])) tem_digito = 1;
+    }
+
+    return tem_maiuscula && tem_minuscula && tem_digito;
+}
+
+int main() {
+    char dado[TAM_BUFFER];
+    char senha[TAM_BUFFER];
+    
+    printf("Digite um CPF/Cartao: ");
+    ler_string(dado, TAM_BUFFER);
+    mascarar_dados(dado);
+    printf("Dado mascarado: %s\n\n", dado);
+    
+    printf("Digite uma senha para validar: ");
+    ler_string(senha, TAM_BUFFER);
+    if (validar_senha(senha)) {
+        printf("Status: SENHA FORTE!\n");
+    } else {
+        printf("Status: SENHA FRACA!\n");
+    }
+    
+    return 0;
+}
 
